0263f0d8159923abce34694df85f84649c0d4d65
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-18 16:12:57 +0300

    BaseEntity kuruldu
    id, orderTİme, updateOrderTİme

--- src/main/java/com/example/enoca_javac/entity/BaseEntity.java
+++ src/main/java/com/example/enoca_javac/entity/BaseEntity.java
@@ -1,4 +1,23 @@
 package com.example.enoca_javac.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @CreationTimestamp
+    private LocalDateTime orderTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updateOrderTime;
 }
