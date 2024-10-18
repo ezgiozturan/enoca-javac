3b18f85ab274b460ef703f873405e57577e612d5
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-18 12:10:42 +0300

    Initial commit

---
+++ src/main/java/com/example/enoca_javac/EnocaJavacApplication.java
@@ -0,0 +1,13 @@
+package com.example.enoca_javac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnocaJavacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnocaJavacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/enoca_javac/EnocaJavacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enoca_javac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnocaJavacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
