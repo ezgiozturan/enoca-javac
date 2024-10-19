0ca7e19f8f27ca1592b3339dff3aba6bf24cd353
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 11:39:20 +0300

    Update Cart entity
    Ürün silinince ve eklenince cartın total fiyatı güncellenir

--- src/main/java/com/example/enoca_javac/entity/Cart.java
+++ src/main/java/com/example/enoca_javac/entity/Cart.java
@@ -1,11 +1,35 @@
 package com.example.enoca_javac.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cart extends BaseEntity{
 
+@Entity
+public class Cart extends BaseEntity {
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Product> products = new ArrayList<>();
 
+    private double totalPrice = 0.0;
 
+    public void addProduct(Product product) {
+        products.add(product);
+        calculateTotalPrice();
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+        calculateTotalPrice();
+    }
+
+    public void calculateTotalPrice() {
+        totalPrice = products.stream()
+                .mapToDouble(Product::getPrice)
+                .sum();
+    }
 }
+
