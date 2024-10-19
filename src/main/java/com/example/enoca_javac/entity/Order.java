5f71f82236d23dbb609d8bc8b06b2790bcee0eb4
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 11:44:10 +0300

    Update Order
    Variablelar oluşturuldu

--- src/main/java/com/example/enoca_javac/entity/Order.java
+++ src/main/java/com/example/enoca_javac/entity/Order.java
@@ -1,4 +1,13 @@
 package com.example.enoca_javac.entity;
 
 public class Order {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order extends BaseEntity{
+    private Customer customer;
+    
+    private List<PreviousOrder> orders = new ArrayList<>();
+
+    private double totalPrice;
 }
