2340492b85ecdcc6a50373f8324d8da584fd0728
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 14:32:17 +0300

    Product Service iş mantığı oluşturuldu
    createProduct, updateProduct, deleteProduct, getProduct

--- src/main/java/com/example/enoca_javac/service/ProductService.java
+++ src/main/java/com/example/enoca_javac/service/ProductService.java
@@ -1,16 +1,42 @@
 package com.example.enoca_javac.service;
 
+import com.example.enoca_javac.dto.ProductDTO;
 import com.example.enoca_javac.entity.Product;
+import com.example.enoca_javac.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
 public class ProductService {
 
-    public Product createProduct(){};
+    @Autowired
+    private ProductRepository productRepository;
 
-    public Product updateProduct(){};
+    public Product createProduct(ProductDTO productDTO){
+        Product product = new Product();
+        product.setName(productDTO.getName());
+        product.setPrice(productDTO.getPrice());
+        product.setStockQuantity(productDTO.getStockQuantity());
+        return productRepository.save(product);
+    };
 
-    public void deleteProduct(){};
+    public Product updateProduct(long productId, ProductDTO productDTO){
+        Product product = productRepository.findById(productId);
+         //Exception eklenecek
+        product.setName(productDTO.getName());
+        product.setPrice(productDTO.getPrice());
+        product.setStockQuantity(productDTO.getStockQuantity());
 
-    public Optional<Product> getProduct(){};
+        return productRepository.save(product);
+
+
+    };
+
+    public void deleteProduct(long productId){
+        productRepository.deleteById(productId);
+    };
+
+    public Optional<Product> getProduct(long productId){
+        return productRepository.findById(productId);
+    };
 }
