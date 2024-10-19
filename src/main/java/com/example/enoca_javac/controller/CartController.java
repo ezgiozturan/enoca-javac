8a7c47929b0db4643cfe62a4954fdb737ead756f
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 15:25:48 +0300

    PutMapping
    Cart güncelleme

--- src/main/java/com/example/enoca_javac/controller/CartController.java
+++ src/main/java/com/example/enoca_javac/controller/CartController.java
@@ -1,11 +1,14 @@
 package com.example.enoca_javac.controller;
 
+import com.example.enoca_javac.dto.CartDTO;
 import com.example.enoca_javac.entity.Cart;
 import com.example.enoca_javac.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class CartController {
 
@@ -18,6 +21,12 @@ public class CartController {
         return ResponseEntity.ok(cart);
     }
 
+
+    @PutMapping("/{cartId}")
+    public ResponseEntity<Cart> updateCart(@PathVariable Long cartId, @RequestBody CartDTO cartDTO) {
+        Cart updatedCart = cartService.updateCart(cartId, cartDTO);
+        return ResponseEntity.ok(updatedCart);
+    }
 
 
 }
