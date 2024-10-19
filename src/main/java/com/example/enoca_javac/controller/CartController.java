c9ae9aa2b5c38984ecd4a207ae530f0caabf48dc
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 15:24:58 +0300

    GetMapping
    Müşteri idsine göre kartını getir-->getCart

--- src/main/java/com/example/enoca_javac/controller/CartController.java
+++ src/main/java/com/example/enoca_javac/controller/CartController.java
@@ -1,4 +1,23 @@
 package com.example.enoca_javac.controller;
 
+import com.example.enoca_javac.entity.Cart;
+import com.example.enoca_javac.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Cart> getCart(@PathVariable long customerId) {
+        Cart cart = cartService.getCart(customerId);
+        return ResponseEntity.ok(cart);
+    }
+
+
+
 }
