0ac0ee9d64c7952f1df191491de8ce13d3c0714d
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 14:41:06 +0300

    Update OrderService.java
    placeOrder, getOrderForCode metodları eklendi

--- src/main/java/com/example/enoca_javac/service/OrderService.java
+++ src/main/java/com/example/enoca_javac/service/OrderService.java
@@ -1,11 +1,48 @@
 package com.example.enoca_javac.service;
 
+import com.example.enoca_javac.entity.Cart;
 import com.example.enoca_javac.entity.Order;
+import com.example.enoca_javac.repository.CartRepository;
+import com.example.enoca_javac.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrderService {
-    public Order plaveOrder(){};
+    @Autowired
+    private OrderRepository orderRepository;
 
-    public Order getOrderForCode(){};
+    @Autowired
+    private CartRepository cartRepository;
+
+    public Order placeOrder(long customerId){
+
+        Cart cart = cartRepository.findByCustomerId(customerId);
+               //EXCEPTİON
+
+        if (cart.getProducts().isEmpty()) {
+            //EXCEPTİON
+        }
+
+        Order order = new Order();
+        order.setCustomer(cart.getCustomer());
+        order.setTotalPrice(cart.getTotalPrice());
+
+        orderRepository.save(order);
+
+        cart.getProducts().clear();
+        cart.calculateTotalPrice();
+        cartRepository.save(cart);
+
+        return order;
+    };
+
+    public Order getOrderForCode(String orderCode){
+        return orderRepository.findByOrderCode(orderCode);
+           //exception
+
+
+    };
 
 
 }
