7a0007991df422d3c48334f8c0940bfb68940ac0
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 14:29:23 +0300

    Update CartRepository.java
    Optional sınıfıyla müşteri idsi bulunmaması durumunda kontrol sağlandı. !!!exception handler geliştirilebilir...

--- src/main/java/com/example/enoca_javac/repository/CartRepository.java
+++ src/main/java/com/example/enoca_javac/repository/CartRepository.java
@@ -4,6 +4,10 @@ import com.example.enoca_javac.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart,Long> {
+
+    Optional<Cart> findByCustomerId(Long customerId);
 }
