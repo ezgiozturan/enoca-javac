77b9916edc12c1c15e73a670479d59fab468ccce
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 14:31:18 +0300

    Cart Service iş mantıkları oluşturuldu
    getCart, updateCart, emptyCart, addProductToCart, removeProductFromCart

--- src/main/java/com/example/enoca_javac/service/CartService.java
+++ src/main/java/com/example/enoca_javac/service/CartService.java
@@ -1,15 +1,80 @@
 package com.example.enoca_javac.service;
 
+import com.example.enoca_javac.dto.CartDTO;
 import com.example.enoca_javac.entity.Cart;
+import com.example.enoca_javac.entity.Product;
+import com.example.enoca_javac.repository.CartRepository;
+import com.example.enoca_javac.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.stream.Collectors;
+
+@Service
 public class CartService {
-    public Cart getCart(){};
 
-    public Cart updateCart(){};
+    @Autowired
+    private CartRepository cartRepository;
 
-    public Cart emptyCart(){};
+    @Autowired
+    private ProductRepository productRepository;
 
-    public  Cart addProductToCart(){};
+    public Cart getCart(long customerId){
+        return cartRepository.findByCustomerId(customerId);
+        //exception eklenecek
+    };
 
-    public Cart removeProductFromCart(){};
+    public Cart updateCart(long cartId, CartDTO cartDTO){
+        Cart cart = cartRepository.findById(cartId);
+        //exception
+
+        cart.setProducts(cartDTO.getProducts().stream().map(dto -> {
+            Product product = productRepository.findById(dto.getProductId());
+            //exception
+            return product;
+        }).collect(Collectors.toList()));
+        cart.calculateTotalPrice();
+        return cartRepository.save(cart);
+
+    };
+
+    public Cart emptyCart(long cartId){
+        Cart cart = cartRepository.findById(cartId);
+        //exception
+
+        cart.getProducts().clear();
+        cart.calculateTotalPrice();
+
+        return cartRepository.save(cart);
+    };
+
+
+    public  Cart addProductToCart(long cartId, long productId){
+        Cart cart = cartRepository.findById(cartId);
+                //exception
+
+        Product product = productRepository.findById(productId);
+                //exception
+
+        if (!product.isAvailable()) {
+            //exception
+        }
+
+        cart.addProduct(product);
+        product.setStockQuantity(product.getStockQuantity() - 1);
+
+        return cartRepository.save(cart);
+
+    };
+
+    public Cart removeProductFromCart(long cartId, long productId){
+        Cart cart = cartRepository.findById(cartId);
+                //exception
+
+        Product product = productRepository.findById(productId);
+        //exception
+
+        cart.removeProduct(product);
+        return cartRepository.save(cart);
+    };
 }
