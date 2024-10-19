bf36c9e3ec7de401c885b3f384e28c070edf6bdf
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 12:10:04 +0300

    CustomerDTO update
    Variablelar, getter,setterlar oluşturuldu

--- src/main/java/com/example/enoca_javac/dto/CustomerDTO.java
+++ src/main/java/com/example/enoca_javac/dto/CustomerDTO.java
@@ -1,4 +1,12 @@
 package com.example.enoca_javac.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String email;
 }
