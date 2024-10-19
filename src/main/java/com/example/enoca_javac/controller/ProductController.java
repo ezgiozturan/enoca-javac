baac5f663c1b36d9436325db2af1a017df0d11d5
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 14:56:47 +0300

    PutMapping
    Ürün güncelleme---> updateProduct

--- src/main/java/com/example/enoca_javac/controller/ProductController.java
+++ src/main/java/com/example/enoca_javac/controller/ProductController.java
@@ -6,10 +6,7 @@ import com.example.enoca_javac.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/java/products")
@@ -24,4 +21,12 @@ public class ProductController {
         Product product = productService.createProduct(productDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable long productId, @RequestBody ProductDTO productDTO) {
+        Product updatedProduct = productService.updateProduct(productId, productDTO);
+        return ResponseEntity.ok(updatedProduct);
+    }
+    
+
 }
