5c57f9939d81d5b1ec7afad0f3279913a3f9f487
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 15:34:25 +0300

    getOrderForCode-GetMapping
    Koduna göre siparişi bul ve dön

--- src/main/java/com/example/enoca_javac/controller/OrderController.java
+++ src/main/java/com/example/enoca_javac/controller/OrderController.java
@@ -6,10 +6,7 @@ import com.example.enoca_javac.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/java/orders")
@@ -22,6 +19,12 @@ public class OrderController {
     public ResponseEntity<Order> placeOrder(@PathVariable long customerId) {
         Order order = orderService.placeOrder(customerId);
         return ResponseEntity.status(HttpStatus.CREATED).body(order);
+    }
+
+    @GetMapping("/{orderCode}")
+    public ResponseEntity<Order> getOrderForCode(@PathVariable String orderCode) {
+        Order order = orderService.getOrderForCode(orderCode);
+        return ResponseEntity.ok(order);
     }
 
 }
