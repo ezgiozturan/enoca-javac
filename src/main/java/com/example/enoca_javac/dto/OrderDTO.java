6a8a8b762ace6f8b6c75618c14e63964511b6bac
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 12:12:50 +0300

    Update OrderDTO.java
    Variablelar,getter,setter oluşturuldu

--- src/main/java/com/example/enoca_javac/dto/OrderDTO.java
+++ src/main/java/com/example/enoca_javac/dto/OrderDTO.java
@@ -1,4 +1,14 @@
 package com.example.enoca_javac.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class OrderDTO {
+    private Long id;
+    private List<ProductDTO> products;
+    private double totalPrice;
 }
