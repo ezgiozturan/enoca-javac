cb18d7c2d2e75e17efb62da4f3c9e986a256f502
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 15:33:17 +0300

    placeOrder-PostMapping
    Yeni sipariş oluşturmak için post isteği

--- src/main/java/com/example/enoca_javac/controller/OrderController.java
+++ src/main/java/com/example/enoca_javac/controller/OrderController.java
@@ -1,4 +1,27 @@
 package com.example.enoca_javac.controller;
 
+
+import com.example.enoca_javac.entity.Order;
+import com.example.enoca_javac.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/java/orders")
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/place/{customerId}")
+    public ResponseEntity<Order> placeOrder(@PathVariable long customerId) {
+        Order order = orderService.placeOrder(customerId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
+    }
+
 }
