1a665bf5f73dda5d9bf516a45297a3eaf0fabc69
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 15:43:18 +0300

    Exception handlerlar oluşturuldu
    gloabal handler ve resource bulunamadı

--- src/main/java/com/example/enoca_javac/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/enoca_javac/exception/ResourceNotFoundException.java
@@ -1,4 +1,15 @@
 package com.example.enoca_javac.exception;
 
 public class ResourceNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
