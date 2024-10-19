f7f025b16d501183408514e1c6e66c0b3d687afd
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 11:49:02 +0300

    Update Order
    ManytoOne ManytoMany ilişkileri kuruldu

--- src/main/java/com/example/enoca_javac/entity/Order.java
+++ src/main/java/com/example/enoca_javac/entity/Order.java
@@ -1,12 +1,20 @@
 package com.example.enoca_javac.entity;
 
 public class Order {
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Order extends BaseEntity{
+    @ManyToOne
     private Customer customer;
-    
+
+    @ManyToMany(cascade = CascadeType.ALL)
     private List<PreviousOrder> orders = new ArrayList<>();
 
     private double totalPrice;
