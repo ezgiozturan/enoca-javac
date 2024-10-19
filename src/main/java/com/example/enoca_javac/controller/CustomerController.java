e01535dc10c44226ddf00384653feeea166390de
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 14:52:14 +0300

    GetMapping eklendi
    Müşterinin tüm siparişleri görüntülenir.

--- src/main/java/com/example/enoca_javac/controller/CustomerController.java
+++ src/main/java/com/example/enoca_javac/controller/CustomerController.java
@@ -3,14 +3,14 @@ package com.example.enoca_javac.controller;
 
 import com.example.enoca_javac.dto.CustomerDTO;
 import com.example.enoca_javac.entity.Customer;
+import com.example.enoca_javac.entity.Order;
 import com.example.enoca_javac.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/java/customers")
@@ -25,6 +25,10 @@ public class CustomerController {
         return ResponseEntity.status(HttpStatus.CREATED).body(customer);
     }
 
-    
+    @GetMapping("/{customerId}/orders")
+    public ResponseEntity<List<Order>> getAllOrdersForCustomer(@PathVariable Long customerId) {
+        List<Order> orders = customerService.getAllOrdersForCustomer(customerId);
+        return ResponseEntity.ok(orders);
+    }
 
 }
