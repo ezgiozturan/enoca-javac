7e52764c789250584f016978b10751cc57d0fd96
ezgiozturan <ezgiozturan05@gmail.com>
2024-10-19 14:55:18 +0300

    PostMapping
    Yeni ürün oluşturma--->createProduct

--- src/main/java/com/example/enoca_javac/controller/ProductController.java
+++ src/main/java/com/example/enoca_javac/controller/ProductController.java
@@ -1,4 +1,27 @@
 package com.example.enoca_javac.controller;
 
+import com.example.enoca_javac.dto.ProductDTO;
+import com.example.enoca_javac.entity.Product;
+import com.example.enoca_javac.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/java/products")
 public class ProductController {
+
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody ProductDTO productDTO) {
+        Product product = productService.createProduct(productDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
 }
